ec49142c2d1aadb1563a1f15f9e9f0680a90804c
skrgns <skrgns0411@naver.com>
2021-05-26 09:05:02 -0700

    data struct edit

--- struct.go
+++ struct.go
@@ -1,52 +1,57 @@
 package main
 
 // video's owner info (user)
-//owner[name] =
+//owner[name] = @
 type owner struct {
-	Name          string           `json:"name"`
-	Videos        map[string]video `json:"videos"`
-	ReputationRaw float64          `json:"reputationRaw"`
-	IsVoter       bool             `json:"isVoter"`
-	Identity      []byte           `json:"identity"`
+	Name          string           `json:"name"`          //username
+	Videos        map[string]video `json:"videos"`        //video list
+	ReputationRaw float64          `json:"reputationRaw"` //reputation raw value
+	IsVoter       bool             `json:"isVoter"`       //is this user owner?
+	Identity      interface{}      `json:"identity"`      //owner cert
 }
 
 // video info
-//video[id] =
+//video[id] = @
 type video struct {
-	Id                      string                  `json:"id"`
-	Owner                   owner                   `json:"owner"`
-	Metadata                string                  `json:"metadata"`
-	ContractInfo            transferContractInfo    `json:"contractInfo"`
+	Id                      string                  `json:"id"`       //video serial
+	Owner                   owner                   `json:"owner"`    //owner info
+	Metadata                string                  `json:"metadata"` //video metadata
+	ContractedInfo          transferContractedInfo  `json:"contractInfo"`
 	ForTransferContractInfo forTransferContractInfo `json:"forTransferContractInfo"`
 }
 
-//contractClass = RF, RE, CC
-type transferContractInfo struct {
-	Contractor   string                  `json:"contractor"` //contract presenter
-	Contractee   string                  `json:"contractee"` //parent video's owner
-	ContractInfo forTransferContractInfo `json:"contractClass"`
+//After Transfer Contract
+//transfer ContractedInfo
+//transferContractInfo[video Id] = @
+type transferContractedInfo struct {
+	Contractor   string                  `json:"contractor"`    //contract presenter
+	Contractee   string                  `json:"contractee"`    //parent video's owner
+	ContractInfo forTransferContractInfo `json:"contractClass"` //for transfer contract Info
 	ParentVideo  string                  `json:"parentVideo"`
 }
 
+//Before Transfer Contract
 //@ RF(royalty free) 		1, fee 0
 //@ RE(royalty exist) 		2, fee exist
-//@ CC(creative commons) 	3 fee 0
+//@ CC(creative commons) 	3, fee 0
+//@@ADD Fee Contract
 type forTransferContractInfo struct {
 	ContractClass int     `json:"contractClass"`
 	ContractFee   float64 `json:"contractFee"`
 }
 
+//Alert Event params
 type contractAlert struct {
-	Contractor []byte `json:"contractor"` //contract presenter
-	Contractee []byte `json:"contractee"` //parent video's owner
-	Video      string `json:"video"`
+	Contractor interface{} `json:"contractor"` //contract presenter
+	Contractee interface{} `json:"contractee"` //parent video's owner
+	Video      string      `json:"video"`
 }
 
 type transferContractWaitingList struct {
-	Contractor []byte `json:"contractor"` //contract presenter
-	Contractee []byte `json:"contractee"` //parent video's owner
-	Video      string `json:"video"`
-	Isfine     bool   `json:"isfine"`
+	Contractor interface{} `json:"contractor"` //contract presenter
+	Contractee interface{} `json:"contractee"` //parent video's owner
+	Video      string      `json:"video"`
+	Isfine     bool        `json:"isfine"`
 }
 
 var contractCount = 0
