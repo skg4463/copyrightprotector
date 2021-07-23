ada012d115f3060a42723ddaa55009484dd6a8df
skg4463 <skg4463>
2021-07-23 17:58:00 +0900

    struct rebuild

--- voting_struct.go
+++ voting_struct.go
@@ -16,3 +16,22 @@ type Party struct {
 	flagProposer  string   `json:"flagProposer"`  //flager
 	repu          int      `json:"repu"`          //repetaion int
 }
+
+//party struct extract to 'voter' and 'content'
+//each Owner that isVoter is true flag have (party)voter struct
+type Voter struct {
+	Id              string   `json:"id"`               //unique serial
+	Name            string   `json:"name"`             //username
+	VotesToAssign   []string `json:"votestoassign"`    //assigned VOTE arr
+	reputationScore int      `json:"reputation_score"` //reputation score for voting score recording
+}
+
+//Voter, Contents boolean class !CHECK!
+
+//each Video have (party)contents struct
+type Contents struct {
+	Id            string   `json:"id"`            //unique serial
+	Name          string   `json:"name"`          //video ID in struct VIDEO
+	votesReceived []string `json:"votesreceived"` //received vote to Voter
+	flagProposer  string   `json:"flag_proposer"` //flag proposer's OWNER namespace
+}
